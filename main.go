c8de094dd763134cecbe14e1f0c1146c314243ef
dudemelo <dudemelo@gmail.com>
2023-07-22 19:12:18 +0200

    Add organization certificates

--- main.go
+++ main.go
@@ -28,12 +28,33 @@ type Client struct {
 	Links   []Link `json:"links"`
 }
 
+type NewCertificate struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+
 type RevokeReason struct {
 	Reason string `json:"reason"`
 }
 
+type OrganizationCertificate struct {
+	Name       string `json:"name"`
+	CommonName string `json:"commonName"`
+	Country    string `json:"country"`
+	Province   string `json:"province"`
+	City       string `json:"city"`
+	Email      string `json:"email"`
+	Department string `json:"department"`
+}
+
 type CaCert struct {
-	Type string `json:"type"`
+	Type         string                  `json:"type"`
+	Organization OrganizationCertificate `json:"organization"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+	Links []Link `json:"links"`
 }
 
 func (c Client) withLinks() Client {
@@ -41,6 +62,7 @@ func (c Client) withLinks() Client {
 		return c
 	}
 	c.Links = []Link{
+		{Rel: "renew", Method: "PATCH", Path: fmt.Sprintf("/clients/%s", c.Name)},
 		{Rel: "revoke", Method: "DELETE", Path: fmt.Sprintf("/clients/%s", c.Name)},
 	}
 	return c
@@ -48,55 +70,81 @@ func (c Client) withLinks() Client {
 
 func main() {
 	r := httprouter.New()
-	r.GET("/", index)
-	r.POST("/ca-certs", postCACert)
-	r.POST("/servers/:name", postServers)
-	r.POST("/clients/:name", postClients)
-	r.DELETE("/clients/:name", deleteClients)
-	r.GET("/clients", getClients)
+	r.POST("/ca-certs", createCACertificate)
+	r.POST("/servers", createServerCertificate)
+	r.POST("/clients", createClientCertificate)
+	r.GET("/certificates", getCertificates)
+	r.PATCH("/certificates/:name", renewCertificate)
+	r.DELETE("/certificates/:name", deleteCertificate)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	http.ServeFile(w, r, "/var/www/index.html")
+func hasCACertificate() bool {
+	_, err := os.Stat("/usr/share/easy-rsa/pki/ca.crt")
+	return err == nil
 }
 
-func postCACert(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	if _, err := os.Stat("/usr/share/easy-rsa/pki/ca.crt"); err == nil {
+func createCACertificate(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	if hasCACertificate() {
 		w.WriteHeader(http.StatusConflict)
+		json.NewEncoder(w).Encode(ErrorResponse{Error: "CA certificate already exists"})
 		return
 	}
+
 	var cert CaCert
 	json.NewDecoder(r.Body).Decode(&cert)
-	if cert.Type == "common" {
-		exec.Command("/usr/share/easy-rsa/easyrsa", "build-ca", "nopass").Run()
-		w.WriteHeader(http.StatusCreated)
+
+	if cert.Type != "common" && cert.Type != "organization" {
+		w.WriteHeader(http.StatusNotImplemented)
+		json.NewEncoder(w).Encode(ErrorResponse{Error: "Invalid CA type"})
 		return
 	}
-	w.WriteHeader(http.StatusNotImplemented)
+
+	cmd := exec.Command("/usr/share/easy-rsa/easyrsa", "build-ca", "nopass")
+
+	if cert.Type == "organization" {
+		cmd.Env = cmd.Environ()
+		cmd.Env = append(cmd.Env, "EASYRSA_DN=org")
+		cmd.Env = append(cmd.Env, fmt.Sprintf("EASYRSA_REQ_ORG=%s", cert.Organization.Name))
+		cmd.Env = append(cmd.Env, fmt.Sprintf("EASYRSA_REQ_CN=%s", cert.Organization.CommonName))
+		cmd.Env = append(cmd.Env, fmt.Sprintf("EASYRSA_REQ_COUNTRY=%s", cert.Organization.Country))
+		cmd.Env = append(cmd.Env, fmt.Sprintf("EASYRSA_REQ_PROVINCE=%s", cert.Organization.Province))
+		cmd.Env = append(cmd.Env, fmt.Sprintf("EASYRSA_REQ_CITY=%s", cert.Organization.City))
+		cmd.Env = append(cmd.Env, fmt.Sprintf("EASYRSA_REQ_EMAIL=%s", cert.Organization.Email))
+		cmd.Env = append(cmd.Env, fmt.Sprintf("EASYRSA_REQ_OU=%s", cert.Organization.Department))
+	}
+
+	cmd.Run()
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(cert)
 }
 
-func postServers(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	exec.Command("/usr/share/easy-rsa/easyrsa", "build-server-full", p.ByName("name"), "nopass").Run()
-	getClients(w, r, p)
+func createServerCertificate(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	var cert NewCertificate
+	json.NewDecoder(r.Body).Decode(&cert)
+	exec.Command("/usr/share/easy-rsa/easyrsa", "build-server-full", cert.Name, cert.Password).Run()
+	getCertificates(w, r, p)
 }
 
-func postClients(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	exec.Command("/usr/share/easy-rsa/easyrsa", "build-client-full", p.ByName("name"), "nopass").Run()
-	getClients(w, r, p)
+func createClientCertificate(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	var cert NewCertificate
+	json.NewDecoder(r.Body).Decode(&cert)
+	exec.Command("/usr/share/easy-rsa/easyrsa", "build-client-full", cert.Name, cert.Password).Run()
+	getCertificates(w, r, p)
 }
 
-func deleteClients(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	var reason RevokeReason
-	json.NewDecoder(r.Body).Decode(&reason)
-	exec.Command("/usr/share/easy-rsa/easyrsa", "revoke", p.ByName("name"), reason.Reason).Run()
-	getClients(w, r, p)
-}
-
-func getClients(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func getCertificates(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	file, err := os.Open("/usr/share/easy-rsa/pki/index.txt")
 	if err != nil {
-		log.Fatal(err)
+		w.WriteHeader(http.StatusTooEarly)
+		json.NewEncoder(w).Encode(ErrorResponse{
+			Error: "No CA certificate found",
+			Links: []Link{
+				{Rel: "create", Method: "POST", Path: "/ca-certs"},
+			},
+		})
+		return
 	}
 	defer file.Close()
 	var clients []Client
@@ -116,4 +164,16 @@ func getClients(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		clients = append(clients, client.withLinks())
 	}
 	json.NewEncoder(w).Encode(clients)
+}
+
+func renewCertificate(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	exec.Command("/usr/share/easy-rsa/easyrsa", "renew", p.ByName("name")).Run()
+	getCertificates(w, r, p)
+}
+
+func deleteCertificate(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	var reason RevokeReason
+	json.NewDecoder(r.Body).Decode(&reason)
+	exec.Command("/usr/share/easy-rsa/easyrsa", "revoke", p.ByName("name"), reason.Reason).Run()
+	getCertificates(w, r, p)
 }
